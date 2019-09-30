8a71f8edf5385e4384766413f59949550337d226
Nelson Arias <nelsonnfaa@gmail.com>
2019-09-30 22:33:27 +0200

    actualizacion de js para abrir el menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 var btnMenu = document.querySelector('.menu__btn');
 var menuPrincipal = document.querySelector('header');
 btnMenu.addEventListener('click', function (event) {
-    alert('hola')
     menuPrincipal.classList.add('verMenu')
 })
-// btnMenu.classList.add('clasePrueba', 'animacion')
