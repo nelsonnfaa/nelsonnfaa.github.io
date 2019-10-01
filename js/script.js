f4ea3015367d74939578acdc2d2afb14d6bc5da6
Nelson Arias <nelsonnfaa@gmail.com>
2019-10-01 21:56:38 +0200

    opacity en menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var btnMenu = document.querySelector('.menu__btn');
-var menuPrincipal = document.querySelector('header');
+var cabecera = document.querySelector('header');
+var menu = document.querySelector('header__menu');
 btnMenu.addEventListener('click', function (event) {
-    /*if (menuPrincipal.classList.contains('verMenu')) {
-        menuPrincipal.classList.toggle('verMenu')
+    /*if (cabecera.classList.contains('verMenu')) {
+        cabecera.classList.toggle('verMenu')
     } else {*/
-    menuPrincipal.classList.toggle('verMenu')
+    cabecera.classList.toggle('verMenu')
+    menu.classList.toggle('header__menu--opacity')
     //}
 })
