f5fc811f9c497b45519ba6d7c12831229137c66a
“Nelson <nelsonnfaa@gmail.com>
2019-10-01 10:00:20 +0200

    estilos para menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var btnMenu = document.querySelector('.menu__btn');
 var menuPrincipal = document.querySelector('header');
 btnMenu.addEventListener('click', function (event) {
-    if (menuPrincipal.classList.contains('verMenu')) {
+    /*if (menuPrincipal.classList.contains('verMenu')) {
         menuPrincipal.classList.toggle('verMenu')
-    } else {
-        menuPrincipal.classList.toggle('verMenu')
-    }
+    } else {*/
+    menuPrincipal.classList.toggle('verMenu')
+    //}
 })
