2cfbfde3c8055e3bf49c949493e76a3259e67271
“Nelson <nelsonnfaa@gmail.com>
2019-10-02 15:57:59 +0200

    arreglo de js para opacity en menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 var btnMenu = document.querySelector('.menu__btn');
 var cabecera = document.querySelector('header');
-var menu = document.querySelector('header__menu');
+var menu = document.querySelector('.header__menu');
 btnMenu.addEventListener('click', function (event) {
-    /*if (cabecera.classList.contains('verMenu')) {
-        cabecera.classList.toggle('verMenu')
-    } else {*/
     cabecera.classList.toggle('verMenu')
     menu.classList.toggle('header__menu--opacity')
-    //}
 })
