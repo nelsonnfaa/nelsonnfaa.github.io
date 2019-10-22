a4b8d1c16c21149d88caca57df54ada55aae801a
Nelson Arias <nelsonnfaa@gmail.com>
2019-10-22 09:33:17 +0200

    validacion formulario

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ btnMenu.addEventListener('click', function (event) {
     cabecera.classList.toggle('verMenu')
     menu.classList.toggle('header__menu--opacity')
 })
+
+/* recoger datos de formulario*/
+let formulario = document.querySelector('.formulario');
+//let formulario = document.forms[0];
+formulario.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    alert(formulario.name.value)
+    return false
+})
